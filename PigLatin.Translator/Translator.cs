ffee097cef843d62dac7d35b461867d93d894fba
benja <benjaminjasperson@gmail.com>
2018-04-16 10:46:03 -0600

    Initial Commit

---
+++ PigLatin.Translator/Translator.cs
@@ -0,0 +1,106 @@
+﻿/********************************************************************
+ * 
+ * This class defines the methods needed for the PigLat translator
+ * 
+ * ******************************************************************/
+using System.Text.RegularExpressions;
+
+
+namespace PigLatin.Translator
+{
+    public class Translator
+    {
+        //All methods are static so no constructor is needed
+
+        private static char[] punctuation = { '!', '?', ',', '.', ':', ';' }; //list of acceptable punctuation
+
+        //public method used to translate each line of text
+        public static string convertToLatin(string line)
+        {
+            string[] words = line.Split(' ');
+            string result = "";
+            for (int i = 0; i < words.Length; i++)
+            {
+                result += translateWord(words[i]) +
+                    (words.Length - 1 == i ? "" : " ");
+            }
+            return result;
+        }
+
+        //used by the convertToLatin method to translate a single word from a line of text
+        private static string translateWord(string word)
+        {
+            //check that the word is translatable
+            if (isTranslatable(word))
+            {
+                string wordWithoutPunc = word.Split(punctuation)[0];
+                string punc = word.Substring(wordWithoutPunc.Length);
+
+                string firstLetter = wordWithoutPunc.Substring(0, 1);
+
+                //Add a 'w' if the word starts with a vowel
+                if (isVowel(firstLetter.ToCharArray()[0]))
+                {
+                    return wordWithoutPunc + "way" + punc;
+                }
+                else
+                {
+                    //Check length
+                    if (wordWithoutPunc.Length < 2)
+                    {
+                        return wordWithoutPunc + "ay" + punc;
+                    }
+                    else
+                    {
+                        string latinWord =
+                            wordWithoutPunc.Substring(1, wordWithoutPunc.Length - 1) +
+                            firstLetter + "ay" + punc;
+
+                        //Check for capitalization
+                        if (firstLetter.ToUpper() == firstLetter)
+                        {
+                            latinWord = fixCapitalization(wordWithoutPunc) + "ay" + punc;
+                        }
+
+                        return latinWord;
+                    }
+                }
+            }
+            else
+            {
+                //If the word contians unexpected characters or is otherwise untranslatable,
+                // return the word as is
+                return word;
+            }
+        }
+
+        
+        private static string fixCapitalization(string word)
+        {
+
+            //Check that word has multiple letters
+            if (word.Length > 1 )
+            {
+                return word.Substring(1, 1).ToUpper() +  //second letter made upper case
+                    (word.Length > 2 ? word.Substring(2,word.Length - 2) : "") + //beyond second if available
+                    word.Substring(0, 1).ToLower(); //first letter made lower case
+            }
+             
+            return null;
+        }
+
+        //Usesd to verify that the starts with letters and ends with punctation.
+        //The word also needs to have at least one character
+        private static bool isTranslatable(string word)
+        {
+             return new Regex("^[A-z]*[[!?.,:;]*$").IsMatch(word) && word.Length > 0;
+
+        }
+
+        //Used to see if a character is a vowel
+        private static bool isVowel(char letter)
+        {
+            return new Regex("[A,a,E,e,I,i,O,o,U,u]+").IsMatch(letter.ToString());
+        }
+    }
+}
---
+++ PigLatin.UI/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿/****************************************************************
+ * 
+ * Program Name: PigLatin
+ * Author: Ben Jasperson
+ * Purpose: Allows user to type text, or select text from a file.
+ *          the text is then translated into pig latin.
+ *   
+ *   **********************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Win32;
+using System.IO;
+
+namespace PigLatin.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //This method will translate text in the input textbox as the user types
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //Check that both boxes have been instantiated and input contains text
+            if (txtInput != null && txtResult != null && txtInput.Text.Length > 0)
+            {
+                txtResult.Text = Translator.Translator.convertToLatin(txtInput.Text);
+            }
+        }
+
+        //Calls the clear text method when clear button pressed
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            ClearText();
+        }
+
+        //Sets text of both text boxes blank
+        private void ClearText()
+        {
+            txtInput.Text = "";
+            txtResult.Text = "";
+        }
+
+        //Allows user to open a file of text to be translated
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            //Initiate OpenFileDialog object
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.RestoreDirectory = true;
+
+            //Declar stream variable
+            Stream stream = null;
+            
+            //Check that a file was selected
+            if ((bool)openFileDialog.ShowDialog())
+            {
+                try
+                {
+                    ClearText();
+
+                    //Check that stream is not null
+                    if ((stream = openFileDialog.OpenFile()) != null)
+                    {
+                        using (StreamReader sr = new StreamReader(stream))
+                        {
+                            //Initiate list to store translated lines
+                            List<string> latin = new List<string>();
+
+                            //Continue reading and translating line until the end of the file is reached
+                            while (!sr.EndOfStream)
+                            {
+                                string line = sr.ReadLine();
+                                txtInput.Text += line + Environment.NewLine;
+                                latin.Add(Translator.Translator.convertToLatin(line));
+                            }
+
+                            //Use list of translated lines to update the result textbox
+                            txtResult.Text = "";
+                            for (int i = 0; i < latin.Count(); i++)
+                            {
+                                txtResult.Text += latin[i] + Environment.NewLine; 
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //Show message if there is an error while opening the file
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+
+        }
+    }
+}
